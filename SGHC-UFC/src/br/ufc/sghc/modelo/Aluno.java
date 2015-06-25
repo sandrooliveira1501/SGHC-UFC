ddcabd27da344a0b0b223d4c891fc68718cee931
sandrooliveira1501 <sandrooliveira1501@gmail.com>
2015-06-24 23:39:39 -0300

    Problema nos pacotes e hibernate resolvido

--- SGHC-UFC/src/br/ufc/sghc/modelo/Aluno.java
+++ SGHC-UFC/src/br/ufc/sghc/modelo/Aluno.java
@@ -1,15 +1,11 @@
 package br.ufc.sghc.modelo;
 
 import java.io.Serializable;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 public class Aluno implements Serializable {
